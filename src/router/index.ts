6c4ba69382bd3d42d566ad67d8a6508f24e07f08
Hadyan Azinudin <hadyan_azinudin@bca.co.id>
2024-07-04 18:23:30 +0700

    test /fb

--- src/router/index.ts
+++ src/router/index.ts
@@ -83,14 +83,14 @@ const router = createRouter({
           // which is lazy-loaded when the route is visited.
           component: () => import("../views/message/UpdateMessagePage.vue"),
         },
-        {
-          path: "/facebook",
-          name: "Facebook",
-          // route level code-splitting
-          // this generates a separate chunk (About.[hash].js) for this route
-          // which is lazy-loaded when the route is visited.
-          component: () => import("../views/facebook/FacebookPage.vue"),
-        },
+        // {
+        //   path: "/facebook",
+        //   name: "Facebook",
+        //   // route level code-splitting
+        //   // this generates a separate chunk (About.[hash].js) for this route
+        //   // which is lazy-loaded when the route is visited.
+        //   component: () => import("../views/facebook/FacebookPage.vue"),
+        // },
         {
           path: "/account",
           name: "Account",
@@ -100,6 +100,14 @@ const router = createRouter({
           component: () => import("../views/account/AccountPage.vue"),
         }
       ]
+    },
+    {
+      path: "/facebook",
+      name: "Facebook",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/facebook/FacebookPage.vue"),
     },
     {
       path: "/sign-in",
